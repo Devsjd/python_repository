3b26de86dc64f2c3def2501c95367b741508da08
sajad <agh.sajad110@gmail.com>
2025-10-28 21:03:54 +0330

    third exercise

--- main.py
+++ main.py
@@ -26,3 +26,23 @@ reverse_num = ones * 10 + tens
 
 # نمایش نتیجه
 print("مقلوب عدد =", reverse_num)
+
+#تمرین سوم
+
+#تمرین سوم
+
+# دریافت عدد دو رقمی
+num = int(input("یک عدد دو رقمی وارد کنید: "))
+
+# جدا کردن رقم دهگان و یکان
+tens = num // 10
+ones = num % 10
+
+# محاسبه توان‌ها
+result1 = tens**ones
+result2 = ones**tens
+result3 = result1+result2
+# نمایش نتیجه
+print("رقم اول به توان رقم دوم =", result1)
+print("رقم دوم به توان رقم اول =", result2)
+print("حاصل جمع دو رقم =" , result3)
