a7cdc7cc3cb63825b92cb6d7947e9ac3b29d0ba0
sajad <agh.sajad110@gmail.com>
2025-10-28 20:48:27 +0330

    first exercise

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+# دریافت سن از کاربر
+age = int(input("سن خود را به سال وارد کنید: "))
+
+# محاسبه بر اساس سال
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+# نمایش نتایج
+print("سن شما به ساعت:", hours)
+print("سن شما به دقیقه:", minutes)
+print("سن شما به ثانیه:", seconds)
