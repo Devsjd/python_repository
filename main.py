820c5ef98cb1b32d60e3cf9d38801e52ea9f4b43
sajad <agh.sajad110@gmail.com>
2025-10-28 20:55:27 +0330

    second exercise

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ seconds = minutes * 60
 print("سن شما به ساعت:", hours)
 print("سن شما به دقیقه:", minutes)
 print("سن شما به ثانیه:", seconds)
+
+#تمرین دوم
+
+# دریافت عدد دو رقمی از کاربر
+num = int(input("یک عدد دو رقمی وارد کنید: "))
+
+# جدا کردن دهگان و یکان
+tens = num // 10
+ones = num % 10
+
+# ساخت عدد معکوس
+reverse_num = ones * 10 + tens
+
+# نمایش نتیجه
+print("مقلوب عدد =", reverse_num)
